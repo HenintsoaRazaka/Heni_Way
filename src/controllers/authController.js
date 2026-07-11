b3bd6105b93907daa6dc7a3700b3ba7220d3bd31
Tsotsola <henintsoarazakarimanana@Gmail.com>
2026-07-11 22:06:40 +0300

    reussi

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -6,6 +6,18 @@ const authController = {
             const {  Nom, Pseudo, email, Telephone , password} = req.body;
             const infosPassagers = { Nom, Pseudo, Telephone };
 
+            const abstractApiKey = process.env.ABSTRACT_API_KEY;
+            const abstractUrl = `https://emailreputation.abstractapi.com/v1/?api_key=${abstractApiKey}&email=${email}`;
+            const response = await fetch(abstractUrl);
+            const dataEmail = await response.json();
+
+            if (!dataEmail.deliverability || dataEmail.deliverability === "undeliverable") {
+                return res.status(400).json({
+                    succes: false,
+                    message: "Désolé, cette adresse e-mail n'existe pas dans la réalité."
+                });
+            }
+
             const utilisateur = await authModel.inscription(email, password, infosPassagers);
 
             return res.status(201).json({
