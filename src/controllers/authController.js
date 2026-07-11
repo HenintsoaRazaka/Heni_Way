96cfdfdfffb88644385f0b404a03a2e4b588ef18
Tsotsola <henintsoarazakarimanana@Gmail.com>
2026-07-11 03:30:03 +0300

    reussi

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,10 +4,9 @@ const authController = {
     inscription: async (req, res) => {
         try {
             const {  Nom, Pseudo, email, Telephone , password} = req.body;
-
             const infosPassagers = { Nom, Pseudo, Telephone };
 
-            const utilisateur = await authModel.inscriptionModel(email, password, infosPassagers);
+            const utilisateur = await authModel.inscription(email, password, infosPassagers);
 
             return res.status(201).json({
                 succes: true,
