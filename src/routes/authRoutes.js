607dde0b28d8f8006646d40adb2b5974c25cc36b
Tsotsola <henintsoarazakarimanana@Gmail.com>
2026-07-16 22:20:17 +0300

    (^U^)

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const verifierSession = require('../middlewares/authMiddlewares');
+const Insertion = require ('../controllers/Insert')
 
 router.post('/inscription', authController.inscription);
 router.post('/connexion', authController.connexion);
 router.post('/Administrateur', authController.connexionAdministrateur);
-router.post('/voitures', VoitureController.ajouterVoiture);
+router.post('/voitures', Insertion.Insert);
 
 module.exports = router;
